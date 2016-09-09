fa777224a96401626bd11b7146b8bb594ba2cb38
Maya_Semina <yokkavi@gmail.com>
2016-09-10 00:00:14 +0300

    add com.epam.maya_semina.java.git/src/test/java/com/epam/maya_semina/java/git/test/SimpleTest.java

--- com.epam.maya_semina.java.git/src/test/java/com/epam/maya_semina/java/git/test/SimpleTest.java
+++ com.epam.maya_semina.java.git/src/test/java/com/epam/maya_semina/java/git/test/SimpleTest.java
@@ -17,12 +17,13 @@ public class SimpleTest {
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "D:\\workspace\\com.epam.maya_semina.lesson_10\\src\\test\\resources\\chromedriver.exe");//"D:\\workspace\\com.epam.maya_semina.lesson_10\\src\\test\\resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void test() {
 		driver.get("https://yandex.ru");
+		System.out.println("hello!");
 		Assert.assertTrue(driver.getTitle().equals("яндекс"));
 	}
 	
