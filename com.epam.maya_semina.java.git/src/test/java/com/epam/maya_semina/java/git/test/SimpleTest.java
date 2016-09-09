efc759344f7ea40b03afa8a726b80eca58ab148a
Maya_Semina <yokkavi@gmail.com>
2016-09-09 23:03:53 +0300

    add com.epam.maya_semina.java.git directory

---
+++ com.epam.maya_semina.java.git/src/test/java/com/epam/maya_semina/java/git/test/SimpleTest.java
@@ -0,0 +1,33 @@
+package com.epam.maya_semina.java.git.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "D:\\workspace\\com.epam.maya_semina.lesson_10\\src\\test\\resources\\chromedriver.exe");//"D:\\workspace\\com.epam.maya_semina.lesson_10\\src\\test\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void test() {
+		driver.get("https://yandex.ru");
+		Assert.assertTrue(driver.getTitle().equals("яндекс"));
+	}
+	
+	@AfterSuite
+	public void close(){
+		driver.close();
+	}
+}
